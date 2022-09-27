6e3c9cc2e597ff93a8f7a78714971ce4db052908
Tej Patel <tejpatel317@gmail.com>
2022-09-27 18:06:09 -0400

    Adds rightbar text with styling

--- src/components/Leftbar.jsx
+++ src/components/Leftbar.jsx
@@ -1,4 +1,4 @@
-import { Box, List, ListItem, ListItemButton, ListItemIcon, ListItemText } from '@mui/material'
+import { Box, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Typography } from '@mui/material'
 import HomeIcon from '@mui/icons-material/Home';
 import AddIcon from '@mui/icons-material/Add';
 import React from 'react'
@@ -10,7 +10,7 @@ function Leftbar() {
     <Box bgcolor="#eeeeee" borderRight={2} borderColor="#bdbdbd" flex={1} p={5} sx={{ display: { xs: "none", sm:"block"}}}>
         <Box position="fixed" className="leftbarinnerbox">
         <img className="companylogo" src="https://th.bing.com/th/id/R.3f7189662f19f8318fc75252deee723a?rik=Qa956Np1tp8Zcg&riu=http%3a%2f%2f1000logos.net%2fwp-content%2fuploads%2f2017%2f06%2fTwitter-Logo.png&ehk=6ekNd2ZmhpvFDGRZF19QcumP9fb8pZRkwrbFbK%2bpULA%3d&risl=&pid=ImgRaw&r=0"/>
-        <h1 className="companyname">Twitter</h1>
+        <Typography variant="h4" className="companyname">Twitter</Typography>
                 <List>
                     <ListItem disablePadding>
                         <ListItemButton component={Link} to="/"> 
--- src/components/Rightbar.jsx
+++ src/components/Rightbar.jsx
@@ -1,53 +1,10 @@
-import { Box } from '@mui/material'
+import { Box, Typography } from '@mui/material'
 import React from 'react'
 
 function Rightbar() {
   return (
     <Box bgcolor="#eeeeee" borderLeft={2} borderColor="#bdbdbd" flex={1} p={2} sx={{ display: { xs: "none", sm:"block"}}}>
-      <h1>Hello</h1>
-      <h1>Hello</h1>
-      <h1>Hello</h1>
-      <h1>Hello</h1>
-      <h1>Hello</h1>
-      <h1>Hello</h1>
-      <h1>Hello</h1>
-      <h1>Hello</h1>
-      <h1>Hello</h1>
-      <h1>Hello</h1>
-      <h1>Hello</h1>
-      <h1>Hello</h1>
-      <h1>Hello</h1>
-      <h1>Hello</h1>
-      <h1>Hello</h1>
-      <h1>Hello</h1>
-      <h1>Hello</h1>
-      <h1>Hello</h1>
-      <h1>Hello</h1>
-      <h1>Hello</h1>
-      <h1>Hello</h1>
-      <h1>Hello</h1>
-      <h1>Hello</h1>
-      <h1>Hello</h1>
-      <h1>Hello</h1>
-      <h1>Hello</h1>
-      <h1>Hello</h1>
-      <h1>Hello</h1>
-      <h1>Hello</h1>
-      <h1>Hello</h1>
-      <h1>Hello</h1>
-      <h1>Hello</h1>
-      <h1>Hello</h1>
-      <h1>Hello</h1>
-      <h1>Hello</h1>
-      <h1>Hello</h1>
-      <h1>Hello</h1>
-      <h1>Hello</h1>
-      <h1>Hello</h1>
-      <h1>Hello</h1>
-      <h1>Hello</h1>
-      <h1>Hello</h1>
-      <h1>Hello</h1>
-      <h1>Hello</h1>
+      <Typography variant="h6" fontWeight={300} className="rightbartext">Employees</Typography>
     </Box>
   )
 }
