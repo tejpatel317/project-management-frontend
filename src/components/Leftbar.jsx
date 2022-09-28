d170bee161fc65cbac14c91de6dc40f55ca3b64a
Tej Patel <tejpatel317@gmail.com>
2022-09-28 17:56:57 -0400

    Completes right bar styling

--- src/components/Leftbar.jsx
+++ src/components/Leftbar.jsx
@@ -1,27 +1,28 @@
-import { Box, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Typography } from '@mui/material'
+import { Box, Button, List, ListItem, ListItemButton, ListItemIcon, ListItemText, makeStyles, styled, Typography } from '@mui/material'
 import HomeIcon from '@mui/icons-material/Home';
 import AddIcon from '@mui/icons-material/Add';
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 import '../index.css'
+
 
 function Leftbar() {
   return (
-    <Box bgcolor="#eeeeee" borderRight={2} borderColor="#bdbdbd" flex={1} p={5} sx={{ display: { xs: "none", sm:"block"}}}>
+    <Box bgcolor="#eeeeee" borderRight={2} borderColor="#bdbdbd" flex={1} p={5} n>
         <Box position="fixed" className="leftbarinnerbox">
         <img className="companylogo" src="https://th.bing.com/th/id/R.3f7189662f19f8318fc75252deee723a?rik=Qa956Np1tp8Zcg&riu=http%3a%2f%2f1000logos.net%2fwp-content%2fuploads%2f2017%2f06%2fTwitter-Logo.png&ehk=6ekNd2ZmhpvFDGRZF19QcumP9fb8pZRkwrbFbK%2bpULA%3d&risl=&pid=ImgRaw&r=0"/>
         <Typography variant="h4" className="companyname">Twitter</Typography>
                 <List>
-                    <ListItem disablePadding>
-                        <ListItemButton component={Link} to="/"> 
+                    <ListItem disablePadding >
+                        <ListItemButton component={NavLink} to="/"> 
                             <ListItemIcon>
                                 <HomeIcon/>
                             </ListItemIcon>
-                            <ListItemText primary="Homepage" />
+                            <ListItemText primary="Homepage"/>
                         </ListItemButton>
                     </ListItem>
                     <ListItem disablePadding>
-                        <ListItemButton component={Link} to="/addproject"> 
+                        <ListItemButton component={NavLink} to="/addproject"> 
                             <ListItemIcon>
                                 <AddIcon/>
                             </ListItemIcon>
@@ -29,7 +30,7 @@ function Leftbar() {
                         </ListItemButton>
                     </ListItem>
                     <ListItem disablePadding>
-                        <ListItemButton component={Link} to="/addemployee"> 
+                        <ListItemButton component={NavLink} to="/addemployee"> 
                             <ListItemIcon>
                                 <AddIcon/>
                             </ListItemIcon>
--- src/components/Rightbar.jsx
+++ src/components/Rightbar.jsx
@@ -1,10 +1,297 @@
-import { Box, Typography } from '@mui/material'
+import { Avatar, Box, Container, List, ListItem, ListItemAvatar, ListItemText, styled, Typography } from '@mui/material'
 import React from 'react'
+
+
+const Search = styled("div")({
+  display: "flex",
+  justifyContent: "space-between",
+  backgroundColor: "white",
+  margin: "20px 10px 0px 10px",
+  padding: "10px 10px",
+  borderRadius: "6px",
+});
 
 function Rightbar() {
   return (
-    <Box bgcolor="#eeeeee" borderLeft={2} borderColor="#bdbdbd" flex={1} p={2} sx={{ display: { xs: "none", sm:"block"}}}>
-      <Typography variant="h6" fontWeight={300} className="rightbartext">Employees</Typography>
+    <Box bgcolor="#eeeeee" borderLeft={2} borderColor="#bdbdbd" flex={1} sx={{ display: { xs: "none", sm:"block"}}}>
+      <div className="rightbarsticky">
+        <Typography variant="h6" fontWeight={400} className="rightbartext" position="sticky" pt={2}>Employees</Typography>
+        <Search>Search</Search>
+      </div>
+      <div className="avatarblock">
+      <List>
+      <ListItem>
+        <ListItemAvatar>
+          <Avatar alt="Remy Sharp" src="https://www.teachaway.com/sites/default/files/profile_picture.jpg" />
+        </ListItemAvatar>
+        <ListItemText primary="THIS ITEM WILL BE A NAME"/>
+      </ListItem>
+      <ListItem>
+        <ListItemAvatar>
+          <Avatar alt="Remy Sharp" src="https://content1.getnarrativeapp.com/68f6039f-a2f9-4e15-9b91-4f7cebe2a378/img_ref/4e80a9d6-aef4-4875-ab94-5952b6926e07/Hamish-5_professional_headshots_photographer_headshot_750.jpg" />
+        </ListItemAvatar>
+        <ListItemText primary="Work"/>
+      </ListItem>
+      <ListItem>
+        <ListItemAvatar>
+          <Avatar alt="Remy Sharp" src="https://i.pinimg.com/736x/97/31/02/9731022f0be7c965e880505461643850.jpg"/>
+        </ListItemAvatar>
+        <ListItemText primary="Vacation"/>
+      </ListItem>
+      <ListItem>
+        <ListItemAvatar>
+          <Avatar alt="Remy Sharp" src="https://www.teachaway.com/sites/default/files/profile_picture.jpg" />
+        </ListItemAvatar>
+        <ListItemText primary="THIS ITEM WILL BE A NAME"/>
+      </ListItem>
+      <ListItem>
+        <ListItemAvatar>
+          <Avatar alt="Remy Sharp" src="https://content1.getnarrativeapp.com/68f6039f-a2f9-4e15-9b91-4f7cebe2a378/img_ref/4e80a9d6-aef4-4875-ab94-5952b6926e07/Hamish-5_professional_headshots_photographer_headshot_750.jpg" />
+        </ListItemAvatar>
+        <ListItemText primary="Work"/>
+      </ListItem>
+      <ListItem>
+        <ListItemAvatar>
+          <Avatar alt="Remy Sharp" src="https://i.pinimg.com/736x/97/31/02/9731022f0be7c965e880505461643850.jpg"/>
+        </ListItemAvatar>
+        <ListItemText primary="Vacation"/>
+      </ListItem>
+      <ListItem>
+        <ListItemAvatar>
+          <Avatar alt="Remy Sharp" src="https://www.teachaway.com/sites/default/files/profile_picture.jpg" />
+        </ListItemAvatar>
+        <ListItemText primary="THIS ITEM WILL BE A NAME"/>
+      </ListItem>
+      <ListItem>
+        <ListItemAvatar>
+          <Avatar alt="Remy Sharp" src="https://content1.getnarrativeapp.com/68f6039f-a2f9-4e15-9b91-4f7cebe2a378/img_ref/4e80a9d6-aef4-4875-ab94-5952b6926e07/Hamish-5_professional_headshots_photographer_headshot_750.jpg" />
+        </ListItemAvatar>
+        <ListItemText primary="Work"/>
+      </ListItem>
+      <ListItem>
+        <ListItemAvatar>
+          <Avatar alt="Remy Sharp" src="https://i.pinimg.com/736x/97/31/02/9731022f0be7c965e880505461643850.jpg"/>
+        </ListItemAvatar>
+        <ListItemText primary="Vacation"/>
+      </ListItem>
+      <ListItem>
+        <ListItemAvatar>
+          <Avatar alt="Remy Sharp" src="https://www.teachaway.com/sites/default/files/profile_picture.jpg" />
+        </ListItemAvatar>
+        <ListItemText primary="THIS ITEM WILL BE A NAME"/>
+      </ListItem>
+      <ListItem>
+        <ListItemAvatar>
+          <Avatar alt="Remy Sharp" src="https://content1.getnarrativeapp.com/68f6039f-a2f9-4e15-9b91-4f7cebe2a378/img_ref/4e80a9d6-aef4-4875-ab94-5952b6926e07/Hamish-5_professional_headshots_photographer_headshot_750.jpg" />
+        </ListItemAvatar>
+        <ListItemText primary="Work"/>
+      </ListItem>
+      <ListItem>
+        <ListItemAvatar>
+          <Avatar alt="Remy Sharp" src="https://i.pinimg.com/736x/97/31/02/9731022f0be7c965e880505461643850.jpg"/>
+        </ListItemAvatar>
+        <ListItemText primary="Vacation"/>
+      </ListItem>
+      <ListItem>
+        <ListItemAvatar>
+          <Avatar alt="Remy Sharp" src="https://www.teachaway.com/sites/default/files/profile_picture.jpg" />
+        </ListItemAvatar>
+        <ListItemText primary="THIS ITEM WILL BE A NAME"/>
+      </ListItem>
+      <ListItem>
+        <ListItemAvatar>
+          <Avatar alt="Remy Sharp" src="https://content1.getnarrativeapp.com/68f6039f-a2f9-4e15-9b91-4f7cebe2a378/img_ref/4e80a9d6-aef4-4875-ab94-5952b6926e07/Hamish-5_professional_headshots_photographer_headshot_750.jpg" />
+        </ListItemAvatar>
+        <ListItemText primary="Work"/>
+      </ListItem>
+      <ListItem>
+        <ListItemAvatar>
+          <Avatar alt="Remy Sharp" src="https://i.pinimg.com/736x/97/31/02/9731022f0be7c965e880505461643850.jpg"/>
+        </ListItemAvatar>
+        <ListItemText primary="Vacation"/>
+      </ListItem>
+      <ListItem>
+        <ListItemAvatar>
+          <Avatar alt="Remy Sharp" src="https://www.teachaway.com/sites/default/files/profile_picture.jpg" />
+        </ListItemAvatar>
+        <ListItemText primary="THIS ITEM WILL BE A NAME"/>
+      </ListItem>
+      <ListItem>
+        <ListItemAvatar>
+          <Avatar alt="Remy Sharp" src="https://content1.getnarrativeapp.com/68f6039f-a2f9-4e15-9b91-4f7cebe2a378/img_ref/4e80a9d6-aef4-4875-ab94-5952b6926e07/Hamish-5_professional_headshots_photographer_headshot_750.jpg" />
+        </ListItemAvatar>
+        <ListItemText primary="Work"/>
+      </ListItem>
+      <ListItem>
+        <ListItemAvatar>
+          <Avatar alt="Remy Sharp" src="https://i.pinimg.com/736x/97/31/02/9731022f0be7c965e880505461643850.jpg"/>
+        </ListItemAvatar>
+        <ListItemText primary="Vacation"/>
+      </ListItem>
+      <ListItem>
+        <ListItemAvatar>
+          <Avatar alt="Remy Sharp" src="https://www.teachaway.com/sites/default/files/profile_picture.jpg" />
+        </ListItemAvatar>
+        <ListItemText primary="THIS ITEM WILL BE A NAME"/>
+      </ListItem>
+      <ListItem>
+        <ListItemAvatar>
+          <Avatar alt="Remy Sharp" src="https://content1.getnarrativeapp.com/68f6039f-a2f9-4e15-9b91-4f7cebe2a378/img_ref/4e80a9d6-aef4-4875-ab94-5952b6926e07/Hamish-5_professional_headshots_photographer_headshot_750.jpg" />
+        </ListItemAvatar>
+        <ListItemText primary="Work"/>
+      </ListItem>
+      <ListItem>
+        <ListItemAvatar>
+          <Avatar alt="Remy Sharp" src="https://i.pinimg.com/736x/97/31/02/9731022f0be7c965e880505461643850.jpg"/>
+        </ListItemAvatar>
+        <ListItemText primary="Vacation"/>
+      </ListItem>
+      <ListItem>
+        <ListItemAvatar>
+          <Avatar alt="Remy Sharp" src="https://www.teachaway.com/sites/default/files/profile_picture.jpg" />
+        </ListItemAvatar>
+        <ListItemText primary="THIS ITEM WILL BE A NAME"/>
+      </ListItem>
+      <ListItem>
+        <ListItemAvatar>
+          <Avatar alt="Remy Sharp" src="https://content1.getnarrativeapp.com/68f6039f-a2f9-4e15-9b91-4f7cebe2a378/img_ref/4e80a9d6-aef4-4875-ab94-5952b6926e07/Hamish-5_professional_headshots_photographer_headshot_750.jpg" />
+        </ListItemAvatar>
+        <ListItemText primary="Work"/>
+      </ListItem>
+      <ListItem>
+        <ListItemAvatar>
+          <Avatar alt="Remy Sharp" src="https://i.pinimg.com/736x/97/31/02/9731022f0be7c965e880505461643850.jpg"/>
+        </ListItemAvatar>
+        <ListItemText primary="Vacation"/>
+      </ListItem>
+      <ListItem>
+        <ListItemAvatar>
+          <Avatar alt="Remy Sharp" src="https://www.teachaway.com/sites/default/files/profile_picture.jpg" />
+        </ListItemAvatar>
+        <ListItemText primary="THIS ITEM WILL BE A NAME"/>
+      </ListItem>
+      <ListItem>
+        <ListItemAvatar>
+          <Avatar alt="Remy Sharp" src="https://content1.getnarrativeapp.com/68f6039f-a2f9-4e15-9b91-4f7cebe2a378/img_ref/4e80a9d6-aef4-4875-ab94-5952b6926e07/Hamish-5_professional_headshots_photographer_headshot_750.jpg" />
+        </ListItemAvatar>
+        <ListItemText primary="Work"/>
+      </ListItem>
+      <ListItem>
+        <ListItemAvatar>
+          <Avatar alt="Remy Sharp" src="https://i.pinimg.com/736x/97/31/02/9731022f0be7c965e880505461643850.jpg"/>
+        </ListItemAvatar>
+        <ListItemText primary="Vacation"/>
+      </ListItem>
+      <ListItem>
+        <ListItemAvatar>
+          <Avatar alt="Remy Sharp" src="https://www.teachaway.com/sites/default/files/profile_picture.jpg" />
+        </ListItemAvatar>
+        <ListItemText primary="THIS ITEM WILL BE A NAME"/>
+      </ListItem>
+      <ListItem>
+        <ListItemAvatar>
+          <Avatar alt="Remy Sharp" src="https://content1.getnarrativeapp.com/68f6039f-a2f9-4e15-9b91-4f7cebe2a378/img_ref/4e80a9d6-aef4-4875-ab94-5952b6926e07/Hamish-5_professional_headshots_photographer_headshot_750.jpg" />
+        </ListItemAvatar>
+        <ListItemText primary="Work"/>
+      </ListItem>
+      <ListItem>
+        <ListItemAvatar>
+          <Avatar alt="Remy Sharp" src="https://i.pinimg.com/736x/97/31/02/9731022f0be7c965e880505461643850.jpg"/>
+        </ListItemAvatar>
+        <ListItemText primary="Vacation"/>
+      </ListItem>
+      <ListItem>
+        <ListItemAvatar>
+          <Avatar alt="Remy Sharp" src="https://www.teachaway.com/sites/default/files/profile_picture.jpg" />
+        </ListItemAvatar>
+        <ListItemText primary="THIS ITEM WILL BE A NAME"/>
+      </ListItem>
+      <ListItem>
+        <ListItemAvatar>
+          <Avatar alt="Remy Sharp" src="https://content1.getnarrativeapp.com/68f6039f-a2f9-4e15-9b91-4f7cebe2a378/img_ref/4e80a9d6-aef4-4875-ab94-5952b6926e07/Hamish-5_professional_headshots_photographer_headshot_750.jpg" />
+        </ListItemAvatar>
+        <ListItemText primary="Work"/>
+      </ListItem>
+      <ListItem>
+        <ListItemAvatar>
+          <Avatar alt="Remy Sharp" src="https://i.pinimg.com/736x/97/31/02/9731022f0be7c965e880505461643850.jpg"/>
+        </ListItemAvatar>
+        <ListItemText primary="Vacation"/>
+      </ListItem>
+      <ListItem>
+        <ListItemAvatar>
+          <Avatar alt="Remy Sharp" src="https://www.teachaway.com/sites/default/files/profile_picture.jpg" />
+        </ListItemAvatar>
+        <ListItemText primary="THIS ITEM WILL BE A NAME"/>
+      </ListItem>
+      <ListItem>
+        <ListItemAvatar>
+          <Avatar alt="Remy Sharp" src="https://content1.getnarrativeapp.com/68f6039f-a2f9-4e15-9b91-4f7cebe2a378/img_ref/4e80a9d6-aef4-4875-ab94-5952b6926e07/Hamish-5_professional_headshots_photographer_headshot_750.jpg" />
+        </ListItemAvatar>
+        <ListItemText primary="Work"/>
+      </ListItem>
+      <ListItem>
+        <ListItemAvatar>
+          <Avatar alt="Remy Sharp" src="https://i.pinimg.com/736x/97/31/02/9731022f0be7c965e880505461643850.jpg"/>
+        </ListItemAvatar>
+        <ListItemText primary="Vacation"/>
+      </ListItem>
+      <ListItem>
+        <ListItemAvatar>
+          <Avatar alt="Remy Sharp" src="https://www.teachaway.com/sites/default/files/profile_picture.jpg" />
+        </ListItemAvatar>
+        <ListItemText primary="THIS ITEM WILL BE A NAME"/>
+      </ListItem>
+      <ListItem>
+        <ListItemAvatar>
+          <Avatar alt="Remy Sharp" src="https://content1.getnarrativeapp.com/68f6039f-a2f9-4e15-9b91-4f7cebe2a378/img_ref/4e80a9d6-aef4-4875-ab94-5952b6926e07/Hamish-5_professional_headshots_photographer_headshot_750.jpg" />
+        </ListItemAvatar>
+        <ListItemText primary="Work"/>
+      </ListItem>
+      <ListItem>
+        <ListItemAvatar>
+          <Avatar alt="Remy Sharp" src="https://i.pinimg.com/736x/97/31/02/9731022f0be7c965e880505461643850.jpg"/>
+        </ListItemAvatar>
+        <ListItemText primary="Vacation"/>
+      </ListItem>
+      <ListItem>
+        <ListItemAvatar>
+          <Avatar alt="Remy Sharp" src="https://www.teachaway.com/sites/default/files/profile_picture.jpg" />
+        </ListItemAvatar>
+        <ListItemText primary="THIS ITEM WILL BE A NAME"/>
+      </ListItem>
+      <ListItem>
+        <ListItemAvatar>
+          <Avatar alt="Remy Sharp" src="https://content1.getnarrativeapp.com/68f6039f-a2f9-4e15-9b91-4f7cebe2a378/img_ref/4e80a9d6-aef4-4875-ab94-5952b6926e07/Hamish-5_professional_headshots_photographer_headshot_750.jpg" />
+        </ListItemAvatar>
+        <ListItemText primary="Work"/>
+      </ListItem>
+      <ListItem>
+        <ListItemAvatar>
+          <Avatar alt="Remy Sharp" src="https://i.pinimg.com/736x/97/31/02/9731022f0be7c965e880505461643850.jpg"/>
+        </ListItemAvatar>
+        <ListItemText primary="Vacation"/>
+      </ListItem>
+      <ListItem>
+        <ListItemAvatar>
+          <Avatar alt="Remy Sharp" src="https://www.teachaway.com/sites/default/files/profile_picture.jpg" />
+        </ListItemAvatar>
+        <ListItemText primary="THIS ITEM WILL BE A NAME"/>
+      </ListItem>
+      <ListItem>
+        <ListItemAvatar>
+          <Avatar alt="Remy Sharp" src="https://content1.getnarrativeapp.com/68f6039f-a2f9-4e15-9b91-4f7cebe2a378/img_ref/4e80a9d6-aef4-4875-ab94-5952b6926e07/Hamish-5_professional_headshots_photographer_headshot_750.jpg" />
+        </ListItemAvatar>
+        <ListItemText primary="Work"/>
+      </ListItem>
+      <ListItem>
+        <ListItemAvatar>
+          <Avatar alt="Remy Sharp" src="https://i.pinimg.com/736x/97/31/02/9731022f0be7c965e880505461643850.jpg"/>
+        </ListItemAvatar>
+        <ListItemText primary="Vacation"/>
+      </ListItem>
+    </List>
+    </div>
     </Box>
   )
 }
